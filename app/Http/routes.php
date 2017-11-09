af5eb88f5df4ff2378b3da42906ed9bb6b3c486f
bleedkaga.ogre <bleedkaga.ogre@gmail.com>
2017-11-09 03:58:34 -0500

    add auth route, move admin route to auth middleware protected

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,9 +24,16 @@
 */
 
 Route::group(['middleware' => ['web']], function () {
+    // Authentication Routes...
+    Route::get('login', 'Auth\AuthController@showLoginForm');
+    Route::post('login', 'Auth\AuthController@login');
+    Route::get('logout', 'Auth\AuthController@logout');
+
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('/post/create', ['as' => 'post.create', 'uses' => 'PostController@create']);
+        Route::put('/post', ['as' => 'post.store', 'uses' => 'PostController@store']);
+    });
     Route::get('/', ['as' => 'home', 'uses' => 'PostController@index']);
-    Route::get('/post/create', ['as' => 'post.create', 'uses' => 'PostController@create']);
     Route::get('/post/{id}', ['as' => 'post.show', 'uses' => 'PostController@show']);
-    Route::put('/post', ['as' => 'post.store', 'uses' => 'PostController@store']);
     Route::put('/comment', ['as' => 'comment.store', 'uses' => 'CommentController@store']);
 });
