fef4f7f3a2d6499a1e6c96661a22f7ed7fcdf8cb
bleedkaga.ogre <bleedkaga.ogre@gmail.com>
2017-11-09 02:58:07 -0500

    complete post.create, post.store

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,36 +1,53 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Post;
+use Validator;
 
 class PostController extends Controller
 {
+
     public function index()
     {
-      $posts = Post::orderBy('created_at','DESC')->paginate(5);
-      
-      return view('index', compact('posts'));
+        $posts = Post::orderBy('created_at', 'DESC')->paginate(5);
+
+        return view('index', compact('posts'));
     }
-  
+
     public function show($id)
     {
-      $post = Post::findOrFail($id);
-      return view('post.show', compact('post'));
+        $post = Post::findOrFail($id);
+        return view('post.show', compact('post'));
     }
-  
+
     public function create()
     {
-      return view('post.create');
+        return view('post.create');
     }
-    
-    public function store()
+
+    public function store(Request $request)
     {
-      return "store";
+        $validator = Validator::make($request->all(), [
+                'title' => 'required',
+                'content' => 'required',
+                ], [
+                'title.required' => '文章標題必填',
+                'content.required' => '文章內文必填',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                    ->back()
+                    ->withErrors($validator)
+                    ->withInput();
+        }
+        $post = new Post();
+        $post->title = $request->get('title');
+        $post->content = $request->get('content');
+        $post->save();
+        return redirect()->to(route('home'))->with('message', '新增文章完成');
     }
-  
 }
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.base')
 
 @section('content')
-create
+<section>
+    <div class="row">
+        <div class="col-md-5">
+            <h3>新增文章</h3>
+            <form method="post" action="{{route('post.store')}}">
+                <input type="hidden" name="_method" value="put">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+              <div class="form-group">
+                <label>文章標題</label>
+                <input type="text" name="title" class="form-control">
+              </div>
+              <div class="form-group">
+                <label>文章內文</label>
+                <textarea name="content" class="form-control" cols="40" rows="10"></textarea>
+              </div>
+                <p align="right">
+                    <button type="submit" class="btn btn-default">送出文章</button>
+                </p>
+            </form>
+        </div>
+    </div>
+</section>
 @endSection
