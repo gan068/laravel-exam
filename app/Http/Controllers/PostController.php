e58dd4c06fc27d3cefba2ca32d53111c74e6d9bb
bleedkaga.ogre <bleedkaga.ogre@gmail.com>
2017-11-09 02:44:45 -0500

    add post order by created_at

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 {
     public function index()
     {
-      $posts = Post::paginate(5);
+      $posts = Post::orderBy('created_at','DESC')->paginate(5);
       
       return view('index', compact('posts'));
     }
