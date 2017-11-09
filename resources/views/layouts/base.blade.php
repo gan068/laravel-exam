982bc217dda75212919c8cff7d37dacbf5c496a0
bleedkaga.ogre <bleedkaga.ogre@gmail.com>
2017-11-09 04:03:37 -0500

    change wording

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -54,16 +54,16 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/login') }}">登入</a></li>
                     @else
                        <li><a href="{{ route('post.create') }}">新增文章</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                Hello: {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>登出</a></li>
                             </ul>
                         </li>
                     @endif
