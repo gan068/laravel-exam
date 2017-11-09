d3f9eb26602b8c81ba706932561fc4688b825963
bleedkaga.ogre <bleedkaga.ogre@gmail.com>
2017-11-09 02:43:53 -0500

    add flash message, add validator

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,23 +1,37 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Post;
 use App\Comment;
+use Validator;
 
 class CommentController extends Controller
 {
+
     public function store(Request $request)
     {
-      $post_id = $request->get('post_id');
-      $post = Post::findOrFail($post_id);
-      $comment = new Comment();
-      $comment->content = $request->get('content');
-      $post->comments()->save($comment);
-      return redirect()->to(route('post.show', $post_id));
+        $validator = Validator::make($request->all(), [
+                'content' => 'required',
+                'post_id' => 'required',
+                ], [
+                'content.required' => '留言內文必填',
+                'post_id.required' => '文章編號必填',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                    ->back()
+                    ->withErrors($validator)
+                    ->withInput();
+        }
+        $post_id = $request->get('post_id');
+        $post = Post::findOrFail($post_id);
+        $comment = new Comment();
+        $comment->content = $request->get('content');
+        $post->comments()->save($comment);
+        return redirect()->to(route('post.show', $post_id))->with('message', '留言完成');
     }
 }
