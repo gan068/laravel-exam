4d2a6667de59d97253b7627c1d8bb144ceb838f7
bleedkaga.ogre <bleedkaga.ogre@gmail.com>
2017-11-09 03:59:21 -0500

    add admin user seed

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+          'email'=>'admin@com.tw',
+          'password' => bcrypt('admin1234')
+        ]);
+    }
+}
